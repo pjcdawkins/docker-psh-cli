5e611eb06b50a99bfa13c7ba51340612c0c848c7
Patrick Dawkins <pjcdawkins@gmail.com>
2017-08-03 13:31:58 +0100

    Verify install with SHA256 hash

--- install-cli.sh
+++ install-cli.sh
@@ -2,14 +2,14 @@
 set -e
 
 version="3.19.0"
-sha1="4fe9b0465ca33513b4cb7b94209314e6845786a5"
+sha256="ca4080182458fb18244bf8d93a36695146a737eb5acd35be56d65161af5a0dd7"
 
 # Install the Platform.sh CLI.
 if [ ! -f /usr/local/bin/platform ] || [[ ! "$(platform --version)" == *"$version" ]]; then
   echo "Downloading the Platform.sh CLI version $version"
   curl -sfSL -o platform.phar https://github.com/platformsh/platformsh-cli/releases/download/v"$version"/platform.phar
-  if [[ "$(shasum platform.phar 2>/dev/null)" != "$sha1"* ]]; then
-    echo "SHA1 checksum mismatch for platform.phar"
+  if [[ "$(shasum -a 256 platform.phar 2>/dev/null)" != "$sha256"* ]]; then
+    echo "SHA256 checksum mismatch for platform.phar"
     exit 1
   fi
   chmod +x platform.phar
