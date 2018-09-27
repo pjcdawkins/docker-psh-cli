e01ba8be68d890815ae73207493b95c2d9facc44
Patrick Dawkins <pjcdawkins@gmail.com>
2018-09-27 19:58:41 +0100

    Update CLI to v3.35.1

--- install-cli.sh
+++ install-cli.sh
@@ -3,8 +3,8 @@ set -e
 
 # See https://github.com/platformsh/platformsh-cli/releases for version numbers
 # and SHA-256 hashes.
-version="3.34.1"
-sha256="53cfbd8255270f9f12b2e5cf9882ea3a9094866bcde19de406ddf676c114d1fa"
+version="3.35.1"
+sha256="187c81f29c387a78af06930ad4d6ca70a34a14062ba464e6e3b5e27208447029"
 
 # Install the Platform.sh CLI.
 if [ ! -f /usr/local/bin/platform ] || [[ ! "$(platform --version)" == *"$version" ]]; then
