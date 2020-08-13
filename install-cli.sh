9098483d9248b331c94095a9ab99b3f8a6c3504f
Patrick Dawkins <pjcdawkins@gmail.com>
2020-08-13 12:27:30 +0100

    Update CLI to 3.61.0

--- install-cli.sh
+++ install-cli.sh
@@ -3,8 +3,8 @@ set -e
 
 # See https://github.com/platformsh/platformsh-cli/releases for version numbers
 # and SHA-256 hashes.
-version="3.60.4"
-sha256="c6eda56990e0f2b14ae50fda61754fdf37d7a4d1f003a0c8707e5ad06730e30a"
+version="3.61.0"
+sha256="6f5114c2a27a3d4631d857cf938eda184bbb431568bfbf006958a6dd3526323f"
 
 # Install the Platform.sh CLI.
 if [ ! -f /usr/local/bin/platform ] || [[ ! "$(platform --version)" == *"$version" ]]; then
