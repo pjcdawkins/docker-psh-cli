344f9f2c9b0b4a0019a1d76315bcc0de80c54580
Patrick Dawkins <patrick@platform.sh>
2023-02-21 20:24:14 +0000

    Update CLI to 4.1.1

--- install-cli.sh
+++ install-cli.sh
@@ -3,8 +3,8 @@ set -e
 
 # See https://github.com/platformsh/platformsh-cli/releases for version numbers
 # and SHA-256 hashes.
-version="4.1.0"
-sha256="23db465209f9bd1ca78d27b08fd958ebcbdfaba8b0bc57ff127faa745fb7b63c"
+version="4.1.1"
+sha256="07956aeb526ae34150432983b08a482c50938362dceff39872e7fb98cc1b33f3"
 
 # Install the Platform.sh CLI.
 if [ ! -f /usr/local/bin/platform ] || [[ ! "$(platform --version)" == *"$version" ]]; then
