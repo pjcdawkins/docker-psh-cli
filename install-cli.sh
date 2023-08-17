cffa949a50db60a9982323118f4e91b09b395097
Patrick Dawkins <patrick@platform.sh>
2023-08-17 12:45:26 -0400

    Update CLI to 4.8.4

--- install-cli.sh
+++ install-cli.sh
@@ -3,8 +3,8 @@ set -e
 
 # See https://github.com/platformsh/legacy-cli/releases for version numbers
 # and SHA-256 hashes.
-version="4.8.3"
-sha256="d27b3dadbfe49873580fa71c886f3a7849cc2c382919467a8321f1630a3f3218"
+version="4.8.4"
+sha256="5cd9ff5919410c30b56b1ffdb0d15980635c45a9d3a278efb85d6a1f40fe29e0"
 
 # Install the Platform.sh CLI.
 if [ ! -f /usr/local/bin/platform ] || [[ ! "$(platform --version)" == *"$version" ]]; then
