7ba34791df4329c988d98fc67bbacc894a922d59
Patrick Dawkins <patrick@platform.sh>
2023-12-29 10:10:34 +0000

    Update CLI to 4.12.1

--- install-cli.sh
+++ install-cli.sh
@@ -3,8 +3,8 @@ set -e
 
 # See https://github.com/platformsh/legacy-cli/releases for version numbers
 # and SHA-256 hashes.
-version="4.12.0"
-sha256="cdd51fcccd6922a946c2356c42e70499e9ccb763b4b88fda47f08a5c1f2824d7"
+version="4.12.1"
+sha256="22cbe0a0be3a3f91e646cbeb6a85fb5e8782b65814ffd6d1ab22bde9a6a87fb7"
 
 # Install the Platform.sh CLI.
 if [ ! -f /usr/local/bin/platform ] || [[ ! "$(platform --version)" == *"$version" ]]; then
